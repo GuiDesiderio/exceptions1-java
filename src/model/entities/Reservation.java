ba17be94555efaa1b7bf63e6af7e64f9008372f6
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-08-22 21:31:41 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -9,9 +9,9 @@ public class Reservation {
 	private Integer roomNumber;
 	private Date chekIn;
 	private Date checkOut;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
+
 	public Reservation(Integer roomNumber, Date chekIn, Date checkOut) {
 		this.roomNumber = roomNumber;
 		this.chekIn = chekIn;
@@ -38,22 +38,22 @@ public class Reservation {
 		long diff = checkOut.getTime() - chekIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	
-	public void updateDates(Date checkIn, Date checkOut) {
+
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates";
+		} if(!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date";
+		}
 		this.chekIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Room "
-				+ roomNumber
-				+ ", check-in: "
-				+ sdf.format(chekIn)
-				+ ", check-out: "
-				+ sdf.format(checkOut)
-				+ ", "
-				+ duration()
-				+ " nights";
+		return "Room " + roomNumber + ", check-in: " + sdf.format(chekIn) + ", check-out: " + sdf.format(checkOut)
+				+ ", " + duration() + " nights";
 	}
 }
